2abc1002ca6cf7efcfaeaeb2c92855fd2caa5e24
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-12-28 17:45:56 +0800

    refactor(app.py & fsm.py): move state settings to class TocMachine
    app.py toc_config -> fsm.py TocMachine.config

--- app.py
+++ app.py
@@ -12,28 +12,7 @@ from utils import send_text_message
 
 load_dotenv()
 
-
-machine = TocMachine(
-    states=["user", "state1", "state2"],
-    transitions=[
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
-        },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
-        },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
-    ],
-    initial="user",
-    auto_transitions=False,
-    show_conditions=True,
-)
+machine = TocMachine()
 
 app = Flask(__name__, static_url_path="")
 
--- fsm.py
+++ fsm.py
@@ -4,8 +4,30 @@ from utils import send_text_message
 
 
 class TocMachine(GraphMachine):
+    configs = {
+        "states": ["user", "state1", "state2"],
+        "transitions": [
+            {
+                "trigger": "advance",
+                "source": "user",
+                "dest": "state1",
+                "conditions": "is_going_to_state1",
+            },
+            {
+                "trigger": "advance",
+                "source": "user",
+                "dest": "state2",
+                "conditions": "is_going_to_state2",
+            },
+            {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+        ],
+        "initial": "user",
+        "auto_transitions": False,
+        "show_conditions": True,
+    }
+
     def __init__(self, **machine_configs):
-        self.machine = GraphMachine(model=self, **machine_configs)
+        self.machine = GraphMachine(model=self, **{**TocMachine.configs, **machine_configs})
 
     def is_going_to_state1(self, event):
         text = event.message.text
