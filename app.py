8fa0180193be3dae87571344bb2ec6771572124a
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-12-28 18:07:35 +0800

    feat(app.py): drop flask-echo sample code
    * rename & replace: webhook_handler() -> callback()
    * BREAKING CHANGE: change the route for webhook_handler()
        * The route has been changed: "/webhook" -> "/callback".
          Please update your LINE bot configuration if the route is used.

--- app.py
+++ app.py
@@ -57,33 +57,6 @@ def callback():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
-
-    return "OK"
-
-
-@app.route("/webhook", methods=["POST"])
-def webhook_handler():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
     app.logger.info(f"Request body: {body}")
 
     # parse webhook body
